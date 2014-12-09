166aee1bb21ee1ce45eac280a9822b64469c7222
Parag Paul <papa@microsoft.com>
2014-12-08 17:29:56 -0800

    some more changes

---
+++ papaprojects/papaiphone2/papaiphone2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  papaiphone2
+//
+//  Created by Sancheeta Paul on 12/8/14.
+//  Copyright (c) 2014 parag paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ papaprojects/papaiphone2/papaiphone2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  papaiphone2
+//
+//  Created by Sancheeta Paul on 12/8/14.
+//  Copyright (c) 2014 parag paul. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
