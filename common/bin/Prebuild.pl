c1d4a327df014ffba65bab074040cf042d7d3f90
Parag Paul <papa@microsoft.com>
2014-12-09 10:10:51 -0800

    Description : made some changes ot the prebuild.pl

--- common/bin/Prebuild.pl
+++ common/bin/Prebuild.pl
@@ -7,5 +7,55 @@ exit main();
 
 sub main
 {
+    my $dir = './papaprojects';
 
+    opendir(DIR, $dir) or die $!;
+    my $jobDir = './builds/jobs/';
+
+    while (my $file = readdir(DIR)) {
+
+        # A file test to check that it is a directory
+	# Use -f to test for a file
+        next unless (-d "$dir/$file");
+        
+        next if ($file =~ m/^\./);
+
+	print "$file\n";
+	my $appParams = "ProjectName=$file\n" .
+	    "ProjectRoot=./papaprojects/$file\n" .
+	    "";
+	my $fileName = $jobDir . $file . '.job';
+	WriteFileText($fileName, $appParams) or die "Unable to write the params required\n";
+
+    }
+	
+}
+
+sub WriteFileText
+{
+    my ($filename, $text) = @_;
+
+    if ($filename)
+    {
+        open(WFTFILE, ">$filename") or return;
+        binmode(WFTFILE, ":utf8");
+        print WFTFILE $text;
+        close(WFTFILE);
+        return 1;
+    }
+    return; #Fail
+}
+
+sub ReadFileText
+{
+    my ($filename) = @_;
+    my $fileText;
+
+    local $/ = undef;
+    open(RFTFILE, "<$filename") or return;
+    binmode(RFTFILE, ":utf8");
+    $fileText = <RFTFILE>;
+    close (RFTFILE);
+
+    return $fileText;
 }
